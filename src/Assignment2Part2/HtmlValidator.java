e87bdd3d81ce9d074f3f90ced8cdd4227a56b696
Chris Payne <chrispayne0215@yahoo.com>
2020-02-01 21:56:02 -0600

    need to fix test case 1

--- src/Assignment2Part2/HtmlValidator.java
+++ src/Assignment2Part2/HtmlValidator.java
@@ -1,8 +1,10 @@
 package Assignment2Part2;
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -12,9 +14,57 @@ public class HtmlValidator {
 	 * Implement this method!
 	 */
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+		// handling of null cases
+		if(tags == null){ return null; }
+		if(tags.isEmpty()){ return null; }
 
+		// create a stack of open tag HTML elements from the tags queue (method argument)
+		Stack<HtmlTag> htmlTagStack = new Stack<>();
+		Stack<HtmlTag> openHtmlTagStack = new Stack<>();
+		Queue<HtmlTag> closingTagQueue = new LinkedList<>();
 
-		return null; // change as needed!
+		// populate a stack with all html tags
+		for(HtmlTag z : tags){
+			htmlTagStack.push(z);
+		}
+
+		// populate a stack of only open tags
+		for(HtmlTag i : tags){
+			if(i.isOpenTag() && !i.isSelfClosing()){
+				openHtmlTagStack.push(i);
+			}
+		}
+
+		// populate a queue of closing tags
+		for(HtmlTag x1 : tags){
+			if(x1.toString().contains("/") && !x1.isSelfClosing()){
+				closingTagQueue.add(x1);
+			}
+		}
+
+		// compare the order of opening and closing tags
+		boolean control = true;
+		while(control){
+			if(openHtmlTagStack.isEmpty()){ break; }
+
+			HtmlTag stackValue = openHtmlTagStack.peek();
+			HtmlTag queueValue = closingTagQueue.peek();
+
+			System.out.println("Comparing: " + stackValue + " to " + queueValue);
+
+			if(stackValue.matches(queueValue)){
+				System.out.println("Stack value: " + stackValue + " matches Queue value: " + queueValue);
+				openHtmlTagStack.pop();
+				closingTagQueue.poll();
+			} else {
+				control = false;
+			}
+		}
+
+		if(openHtmlTagStack.isEmpty() && !closingTagQueue.isEmpty()){
+			return null;
+		}
+		return openHtmlTagStack; // change as needed!
 	}
 	
 	/*
--- src/Assignment2Part2/HtmlValidatorRunner.java
+++ src/Assignment2Part2/HtmlValidatorRunner.java
@@ -1,4 +1,37 @@
 package Assignment2Part2;
 
+import java.io.IOException;
+import java.util.Queue;
+import java.util.Stack;
+
 public class HtmlValidatorRunner {
+    public static void main(String[] args) throws IOException {
+        HtmlValidator h = new HtmlValidator();
+        Stack<HtmlTag> s = new Stack<>();
+
+        System.out.println("Q1 Output:");
+        Queue<HtmlTag> q1 = h.getTagsFromHtmlFile("C:\\Users\\payne\\Desktop\\MCIT\\MCIT594\\src\\Assignment2Part2\\test1.html");
+        Stack<HtmlTag> s1 = h.isValidHtml(q1) ;
+        for(HtmlTag z : s1){
+            System.out.println(z);
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Q2 Output:");
+        Queue<HtmlTag> q2 = h.getTagsFromHtmlFile("C:\\Users\\payne\\Desktop\\MCIT\\MCIT594\\src\\Assignment2Part2\\test2.html");
+        Stack<HtmlTag> s2 = h.isValidHtml(q2);
+        for(HtmlTag z : s2){
+            System.out.println(z);
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Q3:");
+        Queue<HtmlTag> q3 = h.getTagsFromHtmlFile("C:\\Users\\payne\\Desktop\\MCIT\\MCIT594\\src\\Assignment2Part2\\test3.html");
+        Stack<HtmlTag> s3 = h.isValidHtml(q3);
+        for(HtmlTag z : s3){
+            System.out.println(z);
+        }
+    }
 }
