12b904e96fed34938f5c22f08f01a84c5740d7cb
Chris Payne <chrispayne0215@yahoo.com>
2020-02-19 20:08:45 -0600

    finished regex validations and sentence object creation

--- src/Assignment3/Analyzer.java
+++ src/Assignment3/Analyzer.java
@@ -1,9 +1,14 @@
 package Assignment3;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Analyzer {
 	
@@ -11,7 +16,75 @@ public class Analyzer {
 	 * Implement this method in Part 1
 	 */
 	public static List<Sentence> readFile(String filename) {
+		// create Path object to format the filename passed to the method
+		Path file = Paths.get(filename);
+		Charset charset = Charset.forName("US-ASCII");
+
+		// create a linked list to store the sentences
+		LinkedList<Sentence> sentences = new LinkedList<>();
+
+		try {
+			// create a buffered reader to pull in the file data
+			BufferedReader reader = Files.newBufferedReader(file, charset);
+			String line = null;
+			// read the file line by line, assigning each line to a new sentence object
+			while((line = reader.readLine()) != null){
+
+				if(isLineValid(line)){
+					int score = getScore(line);
+					String text = getText(line);
+					Sentence currentSentence = new Sentence(score, text);
+					sentences.add(currentSentence);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return null;
+	}
+
+	// method to test the line is in the valid format
+	public static boolean isLineValid(String line){
+		// regex to test if the line begins with a hyphen (to represent a negative number), or begins with a digit
+		// if the above is true, test to see that the format is "score _space_ character"
+		Pattern p = Pattern.compile("(?=(?=[-])|(?=[\\d]))(^[^\\s]+)(\\s)([a-zA-z])");
+		Matcher m = p.matcher(line);
+
+		if(m.find()){
+			int score = getScore(line);
+			if(Math.abs(score) <= 2){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// create a method to parse the score from the line
+	public static int getScore(String line){
+		// use regex to parse everything up to the first space in the line
+		Pattern p = Pattern.compile("(^[^\\s]+)");
+		Matcher m = p.matcher(line);
+		int score = -99;
+
+		if(m.find()){
+			score = Integer.parseInt(m.group());
+		}
+
+		return score;
+	}
+
+	// create a method to parse the text from the line
+	public static String getText(String line){
+		Pattern p = Pattern.compile("(?<=\\s).*");
+		Matcher m = p.matcher(line);
+
+		if(m.find()){
+			System.out.println("Line matches: " + m.group());
+		}
+
+		return m.group();
 	}
 	
 	/*
