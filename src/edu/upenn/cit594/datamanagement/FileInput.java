225cbd32bce16763874605c2c4bcc0b1af0385ec
Chris Payne <chrispayne0215@yahoo.com>
2020-04-11 18:23:18 -0500

    updated packages to n-tier arcitechture setup per assignment instructions

--- src/edu/upenn/cit594/datamanagement/FileInput.java
+++ src/edu/upenn/cit594/datamanagement/FileInput.java
@@ -1,15 +1,13 @@
-package Module11;
+package edu.upenn.cit594.datamanagement;
 
-import org.json.simple.*;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class FileInput {
 
@@ -29,31 +27,15 @@ public class FileInput {
         String date;
         String tweetText;
 
-        public String getCoordinates() {
-            return coordinates;
-        }
-
-        public String getDate() {
-            return date;
-        }
-
-        public String getTweetText() {
-            return tweetText;
-        }
-
-        public String getIdentifier() {
-            return identifier;
-        }
-    }
-
     public ArrayList<JSONCreateObject> tabDelimitedInput(String filName) throws IOException, ParseException {
         BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\payne\\Desktop\\MCIT594\\src\\Module11\\flu_tweets.txt"));
         ArrayList<JSONCreateObject> tweetArray = new ArrayList<>();
 
 
-        while (reader.read() != -1) {
+        while (reader.readLine() != null) {
             JSONCreateObject obj = new JSONCreateObject();
             String currentTweet = reader.readLine();
+            System.out.println("Current text: " + currentTweet);
             String[] currentTweetArray = currentTweet.split("\t");
             JSONParser parser = new JSONParser();
             obj.coordinates = currentTweetArray[0];
@@ -69,5 +51,22 @@ public class FileInput {
         }
 
         return tweetArray;
+    }
+
+        public String getCoordinates() {
+            return coordinates;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getTweetText() {
+            return tweetText;
+        }
+
+        public String getIdentifier() {
+            return identifier;
+        }
     }
 }
