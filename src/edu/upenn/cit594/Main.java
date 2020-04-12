e130333aee5c39870e87ba76d7f5c56f015f0a78
Chris Payne <chrispayne0215@yahoo.com>
2020-04-11 20:38:45 -0500

    cleaned up the main method

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,13 +1,9 @@
 package edu.upenn.cit594;
-import edu.upenn.cit594.processor.FileInput;
-import org.json.simple.parser.ParseException;
-
-
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args){
         // error handling -- if the below class encounters any errors, the program will exit
         CLIArgsErrorHandling errors = new CLIArgsErrorHandling(args);
+
     }
 }
